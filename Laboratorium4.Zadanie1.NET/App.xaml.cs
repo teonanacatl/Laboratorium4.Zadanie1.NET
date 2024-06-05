7e0e6e298612f11a00d62b4fc19850ef2d27d772
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-05 20:17:34 +0200

    Initial setup of the Laboratorium4.Zadanie1.NET project

---
+++ Laboratorium4.Zadanie1.NET/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Laboratorium4.Zadanie1.NET;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
