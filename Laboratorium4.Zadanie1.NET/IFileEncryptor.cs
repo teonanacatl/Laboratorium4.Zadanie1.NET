95b0a195dc8f57ec028face34b4c1c3de14b8205
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:34:48 +0200

    Added IFileEncryptor interface for file encryption and decryption.

---
+++ Laboratorium4.Zadanie1.NET/IFileEncryptor.cs
@@ -0,0 +1,7 @@
+﻿namespace Laboratorium4.Zadanie1.NET;
+
+public interface IFileEncryptor
+{
+    void EncryptFile(string inputFilePath, string outputFilePath);
+    void DecryptFile(string inputFilePath, string outputFilePath);
+}
