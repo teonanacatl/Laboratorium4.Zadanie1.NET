615aff870cebc2a0f98f870e693e3515aa30e580
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:36:01 +0200

    Added ErrorHandler.cs for logging exceptions to a file in Laboratorium4.Zadanie1.NET project

---
+++ Laboratorium4.Zadanie1.NET/ErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace Laboratorium4.Zadanie1.NET;
+
+public static class ErrorHandler
+{
+    private static readonly string LogFilePath = "error.log";
+
+    public static void LogError(Exception ex)
+    {
+        try
+        {
+            File.AppendAllText(LogFilePath,
+                $"{DateTime.Now}: {ex.Message}{Environment.NewLine}{ex.StackTrace}{Environment.NewLine}");
+        }
+        catch
+        {
+            // Ignored, as we cannot do much if logging fails
+        }
+    }
+}
