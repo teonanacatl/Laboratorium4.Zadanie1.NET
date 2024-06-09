d7e953fcf5ecab5f4ea1701c2cf8508a366acd8f
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:35:23 +0200

    Added AppConfig static class for application-wide configuration settings
    This commit introduces the static class AppConfig.cs which manages configuration settings for the Laboratorium4.Zadanie1.NET project. This includes definitions for the server IP address and port, as well as methods to get the application root directory and paths for encryption settings, file storage, and temporary files.

---
+++ Laboratorium4.Zadanie1.NET/AppConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Net;
+
+namespace Laboratorium4.Zadanie1.NET;
+
+public static class AppConfig
+{
+    public const int Port = 5000;
+    public static readonly IPAddress ServerIp = IPAddress.Parse("127.0.0.1");
+
+    public static string EncryptionSettingsFilePath => Path.Combine(GetAppRootDirectory(), "encryptionSettings.json");
+
+    public static string GetAppRootDirectory()
+    {
+        var exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        return Path.GetDirectoryName(exePath);
+    }
+
+    public static string GetFileStoragePath()
+    {
+        var rootDirectory = GetAppRootDirectory();
+        var fileStoragePath = Path.Combine(rootDirectory, "FileStorage");
+
+        if (!Directory.Exists(fileStoragePath)) Directory.CreateDirectory(fileStoragePath);
+
+        return fileStoragePath;
+    }
+
+    public static string GetTempDirectory()
+    {
+        var rootDirectory = GetAppRootDirectory();
+        var tempDirectory = Path.Combine(rootDirectory, "TempFiles");
+
+        if (!Directory.Exists(tempDirectory)) Directory.CreateDirectory(tempDirectory);
+
+        return tempDirectory;
+    }
+}
