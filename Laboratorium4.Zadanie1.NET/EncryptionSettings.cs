a77eadbc3761be6570a66a0666e4bc889bbed3ee
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:36:15 +0200

    Added new EncryptionSettings class in Laboratorium4.Zadanie1.NET

---
+++ Laboratorium4.Zadanie1.NET/EncryptionSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Laboratorium4.Zadanie1.NET;
+
+public class EncryptionSettings
+{
+    public string EncryptionMethod { get; init; }
+    public byte[] Key { get; init; }
+    public byte[] IV { get; init; }
+}
