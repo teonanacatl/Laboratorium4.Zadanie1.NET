94840b531edaaafb12dd340b0f65689a52b581f8
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:36:28 +0200

    Added new FileSelectionWindow in .NET project
    - A new XAML window has been added for file selection functionality.
    - The window contains a list box for displaying available files and select/deselect files.
    - Two buttons are added for selecting files and canceling the operation.
    - In the code-behind (FileSelectionWindow.xaml.cs), methods are implemented for loading files into the list box, handling select and cancel button clicks.
    - The selected files are stored in a public property for further use."

---
+++ Laboratorium4.Zadanie1.NET/FileSelectionWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Laboratorium4.Zadanie1.NET;
+
+public partial class FileSelectionWindow
+{
+    public List<string> SelectedFiles { get; private set; }
+
+    public FileSelectionWindow(List<string> availableFiles)
+    {
+        InitializeComponent();
+        LoadFileList(availableFiles);
+    }
+
+    private void LoadFileList(List<string> availableFiles)
+    {
+        FilesListBox.Items.Clear();
+        foreach (var file in availableFiles) FilesListBox.Items.Add(file);
+        Console.WriteLine("Files loaded into selection window: " + string.Join(", ", availableFiles));
+    }
+
+    private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+    {
+        SelectedFiles = FilesListBox.SelectedItems.Cast<string>().ToList();
+        Console.WriteLine("Selected files: " + string.Join(", ", SelectedFiles));
+        DialogResult = true;
+        Close();
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+}
