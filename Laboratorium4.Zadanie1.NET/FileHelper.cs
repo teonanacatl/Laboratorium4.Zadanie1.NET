be0bb6b8fe089a1fcd72d9c44a2a91cb9dbf36f9
teonanacatl <przemyslaw.lewalski@gmail.com>
2024-06-09 16:36:38 +0200

    Added FileHelper and FileTransferManager classes for file-related operations. These new classes provide functionalities such as creating a unique filename, retrieving the original filename, uploading, and retrieving files asynchronously. Also included is a method for getting a list of available files.

---
+++ Laboratorium4.Zadanie1.NET/FileHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Laboratorium4.Zadanie1.NET;
+
+public static class FileHelper
+{
+    public static string GetUniqueFilePath(string directory, string fileName)
+    {
+        var uniqueFileName = $"{Guid.NewGuid()}_{fileName}";
+        return Path.Combine(directory, uniqueFileName);
+    }
+
+    public static string GetOriginalFileName(string uniqueFilePath)
+    {
+        var fileName = Path.GetFileName(uniqueFilePath);
+        var originalFileName = fileName[(fileName.IndexOf('_') + 1)..];
+        return originalFileName;
+    }
+}
---
+++ Laboratorium4.Zadanie1.NET/FileTransferManager.cs
@@ -0,0 +1,21 @@
+﻿namespace Laboratorium4.Zadanie1.NET;
+
+public class FileTransferManager(AsyncClient asyncClient)
+{
+    private readonly AsyncClient _asyncClient = asyncClient ?? throw new ArgumentNullException(nameof(asyncClient));
+
+    public async Task<bool> UploadFileAsync(string filePath, string originalFileName)
+    {
+        return await _asyncClient.SendFileAsync(filePath, originalFileName);
+    }
+
+    public async Task<string> RetrieveFileAsync(string fileName)
+    {
+        return await _asyncClient.RetrieveFileAsync(fileName);
+    }
+
+    public async Task<IEnumerable<string>> GetAvailableFilesAsync()
+    {
+        return await _asyncClient.RetrieveFileListAsync();
+    }
+}
